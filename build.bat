193f40b55de97a077af013650dc42c2e8203d813
Galen Senogles <galen.senogles@gmail.com>
2015-12-06 20:31:42 -0800

    Remove some files, add some files, observe

--- build.bat
+++ build.bat
@@ -2,4 +2,5 @@ rem c:\Python27\python.exe dnschef.py --fakeip=127.0.0.1 --fakedomains=google.co
 rem c:\Python34\python.exe c:\Python34\Scripts\cxfreeze yadns.py --target-dir dist
 rem c:\Python34\python.exe setup-py2exe.py py2exe
 rem c:\Python27\python.exe setup-py2exe.py py2exe
-c:\Python27\python.exe setup-py2exe-icon.py py2exe
+rem c:\Python27\python.exe setup-py2exe-icon.py py2exe
+c:\Python27\Scripts\pyinstaller.exe --icon yaicon.ico --noconsole yadns.py
